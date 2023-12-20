70bd1325aca23c4292aecacbb902938a5ff2fd18
haseeb360024 <muhammedhaseeb315@gmail.com>
2023-12-20 11:29:56 +0530

    logout logic and product details

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit { 
+  loginUsername:string = ""
+
+  constructor(private router:Router){}
+  ngOnInit(): void {
+    if(sessionStorage.getItem("username")){
+      this.loginUsername = sessionStorage.getItem("username") || ""
+    }else{
+      this.loginUsername = ""
+    }
+    
+  }
+  logout(){
+    sessionStorage.removeItem("username")
+    sessionStorage.removeItem("token")
+    this.loginUsername = ""
+    this.router.navigateByUrl('')
+  }
 
 }
--- src/app/view-product/view-product.component.ts
+++ src/app/view-product/view-product.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToasterService } from '../services/toaster.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-product',
   templateUrl: './view-product.component.html',
   styleUrls: ['./view-product.component.css']
 })
-export class ViewProductComponent {
+export class ViewProductComponent implements OnInit {
+
+  constructor(private toastr:ToasterService,private route:ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((res:any)=>{
+      console.log(res);
+      const {id} = res
+      // get details of perticular product
+      
+    })
+  }
+
+  addToWishlist(product:any){
+    if(sessionStorage.getItem("token")){
+      this.toastr.showSuccess("proceed to add item to wishlist")
+    }else{
+      this.toastr.showWarning("Operation denied.. PLease login")
+    }
+  }
+
+  addToCart(product:any){
+    if(sessionStorage.getItem("token")){
+      this.toastr.showSuccess("proceed to add item to cart")
+    }else{
+      this.toastr.showWarning("Operation denied.. PLease login")
+    }
+  }
+  
 
 }
