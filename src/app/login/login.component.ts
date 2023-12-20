8e9d0d6c5d279600b98edf3c65ddb8d40bf362ef
haseeb360024 <muhammedhaseeb315@gmail.com>
2023-12-20 10:36:17 +0530

    login logic

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ToasterService } from '../services/toaster.service';
+import { Router } from '@angular/router';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +11,34 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  loginForm = this.fb.group({
+    email:['',[Validators.required,Validators.email]],
+    password:['',[Validators.required,Validators.pattern('[a-zA-Z0-9]*')]]
+  })
+  constructor (private fb:FormBuilder,private toaster:ToasterService,private router:Router,private api:ApiService){}
+
+  login(){
+    if(this.loginForm.valid){
+      const password = this.loginForm.value.password
+      const email = this.loginForm.value.email
+      const user = {password,email}
+
+      this.api.loginAPI(user).subscribe({
+        next:(res:any)=>{
+          this.toaster.showSuccess(`${res.existingUser.username} login successfully`)
+          sessionStorage.setItem("username",res.existingUser.username)
+          sessionStorage.setItem("token",res.token)
+          this.loginForm.reset()
+          this.router.navigateByUrl("")
+        },
+        error:(err:any)=>{
+          this.toaster.showWarning(err.error)
+          this.loginForm.reset()
+        }
+      })
+      
+    }else{
+      this.toaster.showWarning('Invalid Input')
+    }
+  }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ToasterService } from '../services/toaster.service';
+import { Router } from '@angular/router';
+import { ApiService } from '../services/api.service';
 
 
 @Component({
@@ -9,10 +12,32 @@ import { FormBuilder } from '@angular/forms';
 })
 export class RegisterComponent {
   registerForm = this.fb.group({
-    username:[''],
-    email:[''],
-    password:['']
+    username:['',[Validators.required,Validators.pattern('[a-zA-Z]*')]],
+    email:['',[Validators.required,Validators.email]],
+    password:['',[Validators.required,Validators.pattern('[a-zA-Z0-9]*')]]
   })
-  constructor(private fb:FormBuilder){}
+  constructor(private fb:FormBuilder,private toaster:ToasterService, private router:Router,private api:ApiService){}
+  register(){
+    if(this.registerForm.valid){
+      const username = this.registerForm.value.username
+      const password = this.registerForm.value.password
+      const email = this.registerForm.value.email
+      const user = {username,password,email}
 
+      this.api.registerAPI(user).subscribe({
+        next:(res:any)=>{
+          this.toaster.showSuccess(`${res.username} register successfully`)
+          this.registerForm.reset()
+          this.router.navigateByUrl('/user/login')
+        },
+        error:(err:any)=>{
+          this.toaster.showWarning(err.error)
+          this.registerForm.reset()
+        }
+      })
+      
+    }else{
+      this.toaster.showWarning('Invalid Input')
+    }
+  }
 }
